49cb6566dd6303aa00d2d5404395f998b496a6ac
Kodak Wang <kodakwang@gmail.com>
2020-05-06 21:57:15 +0800

    + Optimize config.

--- source/ffconf.h
+++ source/ffconf.h
@@ -68,7 +68,7 @@
 / Locale and Namespace Configurations
 /---------------------------------------------------------------------------*/
 
-#define FF_CODE_PAGE	932
+#define FF_CODE_PAGE	0//932
 /* This option specifies the OEM code page to be used on the target system.
 /  Incorrect code page setting can cause a file open failure.
 /
@@ -97,7 +97,7 @@
 */
 
 
-#define FF_USE_LFN		0
+#define FF_USE_LFN		3//0
 #define FF_MAX_LFN		255
 /* The FF_USE_LFN switches the support for LFN (long file name).
 /
